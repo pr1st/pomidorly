0ef250f4e71d2509d59dfdb4e733f801dd31fd8d
Dmitry Dymov <dymovst@gmail.com>
2019-12-02 00:28:16 +0700

    done current tasks reducer tests

---
+++ frontend/src/reducers/__tests__/currentTasks.test.ts
@@ -0,0 +1,42 @@
+import {CurrentTasksState, GET_CURRENT_TASKS, GetCurrentTasksAction} from "../../types/currentTasks";
+import {currentTasks} from "../currentTasks";
+
+
+describe("Test current tasks reducers", () => {
+    let state : CurrentTasksState;
+    beforeEach(() => {
+        state = [
+            {
+                id: 1,
+                tag: "tag",
+                description: "desc",
+                inQueue:2,
+                numberOfPomidors:5
+            }
+        ];
+    });
+
+    it("get current tasks action", () => {
+        const action : GetCurrentTasksAction = {
+            type: GET_CURRENT_TASKS,
+            tasks: [
+                {
+                    id: 5,
+                    tag: "tag2",
+                    description: "desc",
+                    inQueue:3,
+                    numberOfPomidors:5
+                }, {
+                    id: 3,
+                    tag: "tag1",
+                    description: "desc",
+                    inQueue:2,
+                    numberOfPomidors:51
+                }
+            ]
+        };
+        // @ts-ignore
+        const newState = currentTasks(state, action);
+        expect(newState).toEqual(action.tasks);
+    });
+});
