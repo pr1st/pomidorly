6e7ee8062177def5af5abac1a5d6ab7cec4e1c2d
Sergey Semenov <lvfx@ya.ru>
2019-12-13 20:16:46 +0700

    Completed Task entity

--- backend/src/main/kotlin/model/Task.kt
+++ backend/src/main/kotlin/model/Task.kt
@@ -4,18 +4,24 @@ import org.jetbrains.exposed.sql.Table
 
 object Tasks : Table() {
     val id = integer("id").primaryKey().autoIncrement()
-    val description = varchar("description", 255)
-//    val userId = (integer("user_id") references Users.id)
+    val description = varchar("description", 4095)
+    val tag = varchar("tag", 255)
+    val numberOfPomidors = integer("number_of_pomidors")
+    val inQueue = integer("in_queue")
+    val userId = (integer("user_id") references Users.id)
 }
 
 data class Task(
     val id: Int,
-    val description: String
-//    val userId: Int
+    val description: String,
+    val tag: String,
+    val numberOfPomidors: Int,
+    val inQueue: Int,
+    val userId: Int
 )
 
 data class NewTask(
-    val id: Int?,
-    val description: String
-//    val userId: Int
+    val description: String,
+    val tag: String,
+    val numberOfPomidors: Int
 )
