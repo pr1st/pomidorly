42078226c94927a3c61ab12565ea05de3e27e308
Sergey Semenov <lvfx@ya.ru>
2019-12-13 20:48:50 +0700

    Completed User entity

--- backend/src/main/kotlin/model/User.kt
+++ backend/src/main/kotlin/model/User.kt
@@ -5,6 +5,23 @@ import org.jetbrains.exposed.sql.Table
 object Users : Table() {
     val id = integer("id").primaryKey().autoIncrement()
     val username = varchar("username", 255)
+    val password = varchar("password", 255)
 }
+
+data class User(
+    val id: Int,
+    val username: String,
+    val password: String
+)
+
+data class NewUser(
+    val username: String,
+    val password: String
+)
+
+data class Token(
+    val token: String,
+    val expiresIn: Int
+)
 
 
