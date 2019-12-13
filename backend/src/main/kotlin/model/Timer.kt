c65bceba91d28d35c079e50a9b457c856b6bc903
Sergey Semenov <lvfx@ya.ru>
2019-12-13 20:45:23 +0700

    Added Timer entity

---
+++ backend/src/main/kotlin/model/Timer.kt
@@ -0,0 +1,31 @@
+package model
+
+import org.jetbrains.exposed.sql.Table
+
+object Timers : Table() {
+    val id = ActiveTasks.integer("id").primaryKey().autoIncrement()
+    val pomidorDuration = integer("pomidor_duration")
+    val shortBreakDuration = integer("short_break_duration")
+    val longBreakDuration = integer("long_break_duration")
+    val numberOfPomidorsBeforeLongBreak = integer("number_of_pomidors_before_long_break")
+    val alarmWhenZero = bool("alarm_when_zero")
+    val userId = (integer("user_id") references Users.id)
+}
+
+data class Timer(
+    val id: Int,
+    val pomidorDuration: Int,
+    val shortBreakDuration: Int,
+    val longBreakDuration: Int,
+    val numberOfPomidorsBeforeLongBreak: Int,
+    val alarmWhenZero: Boolean,
+    val userId: Int
+)
+
+data class NewTimer(
+    val pomidorDuration: Int,
+    val shortBreakDuration: Int,
+    val longBreakDuration: Int,
+    val numberOfPomidorsBeforeLongBreak: Int,
+    val alarmWhenZero: Boolean
+)
