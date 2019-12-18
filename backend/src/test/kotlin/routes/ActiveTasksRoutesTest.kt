cd3cb393b0a2228e5c7ae71be814e90770f6dfa1
Sergey Semenov <lvfx@ya.ru>
2019-12-18 22:54:39 +0700

    Added active tasks DELETE and Swap methods tests

--- backend/src/test/kotlin/routes/ActiveTasksRoutesTest.kt
+++ backend/src/test/kotlin/routes/ActiveTasksRoutesTest.kt
@@ -39,7 +39,7 @@ class ActiveTasksRoutesTest : ServerTest() {
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(task)
-                .`when`()
+                .When()
                 .post("/current/tasks")
                 .then()
                 .statusCode(HttpStatusCode.Unauthorized.value)
@@ -55,7 +55,7 @@ class ActiveTasksRoutesTest : ServerTest() {
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(invalidTask)
-                .`when`()
+                .When()
                 .post("/current/tasks")
                 .then()
                 .statusCode(HttpStatusCode.BadRequest.value)
@@ -67,20 +67,20 @@ class ActiveTasksRoutesTest : ServerTest() {
         withToken("Bob", "qwerty") { token ->
             val task1 = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
             val task2 = ActiveTaskDTO(null, "tag2", "desc2", 4, null)
-            val created1 = addActiveTask(task1, token)
-            val created2 = addActiveTask(task2, token)
+            val addedTask1 = addActiveTask(task1, token)
+            val addedTask2 = addActiveTask(task2, token)
 
             val allTasks = given()
                 .header("Token", token)
                 .accept(ContentType.JSON)
-                .`when`()
+                .When()
                 .get("/current/tasks")
                 .then()
                 .contentType(ContentType.JSON)
                 .statusCode(HttpStatusCode.OK.value)
                 .extract().to<Array<ActiveTaskDTO>>()
 
-            assertThat(setOf(created1, created2)).isEqualTo(allTasks.toSet())
+            assertThat(setOf(addedTask1, addedTask2)).isEqualTo(allTasks.toSet())
         }
     }
 
@@ -95,7 +95,7 @@ class ActiveTasksRoutesTest : ServerTest() {
                 .header("Token", token)
                 .contentType(ContentType.JSON)
                 .body(taskForUpdate)
-                .`when`()
+                .When()
                 .put("/current/tasks/{id}", current.id!!)
                 .then()
                 .statusCode(HttpStatusCode.NoContent.value)
@@ -120,11 +120,97 @@ class ActiveTasksRoutesTest : ServerTest() {
                 .header("Token", token)
                 .contentType(ContentType.JSON)
                 .body(taskForUpdate)
-                .`when`()
+                .When()
                 .put("/current/tasks/{id}", invalidId)
                 .then()
                 .statusCode(HttpStatusCode.NotFound.value)
         }
     }
 
+    @Test
+    fun deleteTaskTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            val taskId = addActiveTask(task, token).id!!
+
+            given()
+                .header("Token", token)
+                .When()
+                .delete("/current/tasks/{id}", taskId)
+                .then()
+                .statusCode(HttpStatusCode.NoContent.value)
+
+            given()
+                .header("Token", token)
+                .accept(ContentType.JSON)
+                .When()
+                .get("/current/tasks/{id}", taskId)
+                .then()
+                .statusCode(HttpStatusCode.NotFound.value)
+        }
+    }
+
+    @Test
+    fun deleteTaskWithInvalidIdTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            addActiveTask(task, token)
+            val invalidTaskId = 12345
+
+            given()
+                .header("Token", token)
+                .When()
+                .delete("/current/tasks/{id}", invalidTaskId)
+                .then()
+                .statusCode(HttpStatusCode.NotFound.value)
+        }
+    }
+
+    @Test
+    fun swapTasksTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task1 = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            val task2 = ActiveTaskDTO(null, "tag2", "desc2", 4, null)
+            val addedTask1 = addActiveTask(task1, token)
+            val addedTask2 = addActiveTask(task2, token)
+
+            given()
+                .header("Token", token)
+                .When()
+                .post("/current/tasks/swap/{id1}/{id2}", addedTask1.id!!, addedTask2.id!!)
+                .then()
+                .statusCode(HttpStatusCode.NoContent.value)
+
+            val updatedTask1 = getActiveTask(addedTask1.id!!, token)
+            val updatedTask2 = getActiveTask(addedTask2.id!!, token)
+
+            assertThat(updatedTask1.tag).isEqualTo(addedTask1.tag)
+            assertThat(updatedTask1.description).isEqualTo(addedTask1.description)
+            assertThat(updatedTask1.numberOfPomidors).isEqualTo(addedTask1.numberOfPomidors)
+            assertThat(updatedTask1.inQueue).isEqualTo(addedTask2.inQueue)
+
+            assertThat(updatedTask2.tag).isEqualTo(addedTask2.tag)
+            assertThat(updatedTask2.description).isEqualTo(addedTask2.description)
+            assertThat(updatedTask2.numberOfPomidors).isEqualTo(addedTask2.numberOfPomidors)
+            assertThat(updatedTask2.inQueue).isEqualTo(addedTask1.inQueue)
+        }
+    }
+
+    @Test
+    fun swapTasksWithInvalidIdTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task1 = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            val task2 = ActiveTaskDTO(null, "tag2", "desc2", 4, null)
+            addActiveTask(task1, token)
+            addActiveTask(task2, token)
+
+            given()
+                .header("Token", token)
+                .When()
+                .post("/current/tasks/swap/{id1}/{id2}", 1234, 5678)
+                .then()
+                .statusCode(HttpStatusCode.NotFound.value)
+        }
+    }
 }
+
