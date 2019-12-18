317593a6bff189f99f3c1632543c9c8bc360e2d8
Sergey Semenov <lvfx@ya.ru>
2019-12-18 20:41:31 +0700

    Added sign in tests

--- backend/src/test/kotlin/routes/RoutesTestUtils.kt
+++ backend/src/test/kotlin/routes/RoutesTestUtils.kt
@@ -3,16 +3,29 @@ package routes
 import io.ktor.http.HttpStatusCode
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import model.TokenDTO
 import model.UserDTO
 
 object RoutesTestUtils {
 
-    fun addUser(user: UserDTO) = given()
+    fun signUp(user: UserDTO) {
+        given()
+            .contentType(ContentType.JSON)
+            .body(user)
+            .`when`()
+            .post("auth/signup")
+            .then()
+            .statusCode(HttpStatusCode.NoContent.value)
+    }
+
+    fun signIn(user: UserDTO): TokenDTO = given()
+        .accept(ContentType.JSON)
         .contentType(ContentType.JSON)
         .body(user)
         .`when`()
-        .post("auth/signup")
+        .post("auth/signin")
         .then()
-        .statusCode(HttpStatusCode.NoContent.value)
-
+        .statusCode(HttpStatusCode.OK.value)
+        .contentType(ContentType.JSON)
+        .extract().`as`(TokenDTO::class.java)
 }
---
+++ backend/src/test/kotlin/routes/UsersRoutesTest.kt
@@ -0,0 +1,99 @@
+package routes
+
+import common.ServerTest
+import io.ktor.http.HttpStatusCode
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import model.TimerDTO
+import model.TokenDTO
+import model.UserDTO
+import org.junit.jupiter.api.Test
+
+class UsersRoutesTest : ServerTest() {
+
+    @Test
+    fun signUpTest() {
+        val user = UserDTO("Bob", "qwerty")
+        given()
+            .contentType(ContentType.JSON)
+            .body(user)
+            .When()
+            .post("auth/signup")
+            .then()
+            .statusCode(HttpStatusCode.NoContent.value)
+    }
+
+    @Test
+    fun signUpUsernameConflictTest() {
+        val user = UserDTO("Bob", "qwerty")
+        RoutesTestUtils.signUp(user)
+        val anotherUser = UserDTO("Bob", "12345")
+        given()
+            .contentType(ContentType.JSON)
+            .body(anotherUser)
+            .When()
+            .post("auth/signup")
+            .then()
+            .statusCode(HttpStatusCode.Conflict.value)
+    }
+
+    @Test
+    fun signUpIncorrectDataFieldsTest() {
+        val invalidUser = TimerDTO(25, 5, 15, 4, true)
+        given()
+            .contentType(ContentType.JSON)
+            .body(invalidUser)
+            .When()
+            .post("auth/signup")
+            .then()
+            .statusCode(HttpStatusCode.BadRequest.value)
+    }
+
+    @Test
+    fun signInTest() {
+        val user = UserDTO("Bob", "qwerty")
+        RoutesTestUtils.signUp(user)
+        val token = given()
+            .accept(ContentType.JSON)
+            .contentType(ContentType.JSON)
+            .body(user)
+            .When()
+            .post("auth/signin")
+            .then()
+            .statusCode(HttpStatusCode.OK.value)
+            .contentType(ContentType.JSON)
+            .extract().to<TokenDTO>()
+
+        assert(token.expiresIn > 0)
+    }
+
+    @Test
+    fun signInIncorrectPasswordTest() {
+        val user = UserDTO("Bob", "qwerty")
+        RoutesTestUtils.signUp(user)
+        val invalidUser = UserDTO("Bob", "invalidPassword")
+        given()
+            .accept(ContentType.JSON)
+            .contentType(ContentType.JSON)
+            .body(invalidUser)
+            .When()
+            .post("auth/signin")
+            .then()
+            .statusCode(HttpStatusCode.NotFound.value)
+    }
+
+    @Test
+    fun signInIncorrectDataFieldsTest() {
+        val user = UserDTO("Bob", "qwerty")
+        RoutesTestUtils.signUp(user)
+        val invalidUser = TimerDTO(25, 5, 15, 4, true)
+        given()
+            .accept(ContentType.JSON)
+            .contentType(ContentType.JSON)
+            .body(invalidUser)
+            .When()
+            .post("auth/signin")
+            .then()
+            .statusCode(HttpStatusCode.BadRequest.value)
+    }
+}
