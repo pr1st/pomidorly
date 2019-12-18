f09e8d78e4c5faa606d98cdae1f6d61455e9b363
Sergey Semenov <lvfx@ya.ru>
2019-12-18 13:30:43 +0700

    Added routes util

--- backend/src/main/kotlin/routes/RoutesUtils.kt
+++ backend/src/main/kotlin/routes/RoutesUtils.kt
@@ -4,21 +4,26 @@ import io.ktor.application.ApplicationCall
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.header
 import io.ktor.response.respond
+import model.User
 import services.UsersService
 
 object RoutesUtils {
 
     private val usersService = UsersService()
 
-    suspend fun withUserId(call: ApplicationCall, block: suspend (userId: Int) -> Unit) {
+    suspend fun withUser(call: ApplicationCall, block: suspend (user: User) -> Unit) {
         val token = call.request.header("Token")
         if (token == null) {
             call.respond(HttpStatusCode.BadRequest)
             return
         }
-        val uid = usersService.getUserId(token)
-        if (uid == null) call.respond(HttpStatusCode.Unauthorized)
-        else block(uid)
+        val user = usersService.getUser(token)
+        if (user == null) call.respond(HttpStatusCode.Unauthorized)
+        else block(user)
+    }
+
+    suspend fun withUserId(call: ApplicationCall, block: suspend (userId: Int) -> Unit) {
+        withUser(call) { user -> block(user.id) }
     }
 
 }
