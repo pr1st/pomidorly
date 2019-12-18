68f9f02a71afdb5127c3ee1cab1b4c08aefe2073
Sergey Semenov <lvfx@ya.ru>
2019-12-18 22:34:42 +0700

    Added active tasks PUT and GET methods tests

--- backend/src/test/kotlin/routes/ActiveTasksRoutesTest.kt
+++ backend/src/test/kotlin/routes/ActiveTasksRoutesTest.kt
@@ -2,7 +2,7 @@ package routes
 
 import common.ServerTest
 import io.ktor.http.HttpStatusCode
-import io.restassured.RestAssured
+import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import model.ActiveTaskDTO
 import model.TimerDTO
@@ -34,7 +34,7 @@ class ActiveTasksRoutesTest : ServerTest() {
         withToken("Bob", "qwerty") { token ->
             val invalidToken = token + "abracadabra"
             val task = ActiveTaskDTO(null, "tag1", "desc1", 4, null)
-            RestAssured.given()
+            given()
                 .header("Token", invalidToken)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
@@ -50,7 +50,7 @@ class ActiveTasksRoutesTest : ServerTest() {
     fun addActiveTaskWithInvalidDataFieldsTest() {
         withToken("Bob", "qwerty") { token ->
             val invalidTask = TimerDTO(25, 5, 15, 4, true)
-            RestAssured.given()
+            given()
                 .header("Token", token)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
@@ -59,6 +59,71 @@ class ActiveTasksRoutesTest : ServerTest() {
                 .post("/current/tasks")
                 .then()
                 .statusCode(HttpStatusCode.BadRequest.value)
+        }
+    }
+
+    @Test
+    fun getAllTasksTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task1 = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            val task2 = ActiveTaskDTO(null, "tag2", "desc2", 4, null)
+            val created1 = addActiveTask(task1, token)
+            val created2 = addActiveTask(task2, token)
+
+            val allTasks = given()
+                .header("Token", token)
+                .accept(ContentType.JSON)
+                .`when`()
+                .get("/current/tasks")
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(HttpStatusCode.OK.value)
+                .extract().to<Array<ActiveTaskDTO>>()
+
+            assertThat(setOf(created1, created2)).isEqualTo(allTasks.toSet())
+        }
+    }
+
+    @Test
+    fun updateTaskTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            val current = addActiveTask(task, token)
+            val taskForUpdate = ActiveTaskDTO(null, "tag_new", "desc_new", 3, null)
+
+            given()
+                .header("Token", token)
+                .contentType(ContentType.JSON)
+                .body(taskForUpdate)
+                .`when`()
+                .put("/current/tasks/{id}", current.id!!)
+                .then()
+                .statusCode(HttpStatusCode.NoContent.value)
+
+            val updatedTask = getActiveTask(current.id!!, token)
+
+            assertThat(taskForUpdate.tag).isEqualTo(updatedTask.tag)
+            assertThat(taskForUpdate.description).isEqualTo(updatedTask.description)
+            assertThat(taskForUpdate.numberOfPomidors).isEqualTo(updatedTask.numberOfPomidors)
+        }
+    }
+
+    @Test
+    fun updateTaskWithInvalidIdTest() {
+        withToken("Bob", "qwerty") { token ->
+            val task = ActiveTaskDTO(null, "tag1", "desc1", 2, null)
+            addActiveTask(task, token)
+            val taskForUpdate = ActiveTaskDTO(null, "tag_new", "desc_new", 3, null)
+            val invalidId = 12345
+
+            given()
+                .header("Token", token)
+                .contentType(ContentType.JSON)
+                .body(taskForUpdate)
+                .`when`()
+                .put("/current/tasks/{id}", invalidId)
+                .then()
+                .statusCode(HttpStatusCode.NotFound.value)
         }
     }
 
